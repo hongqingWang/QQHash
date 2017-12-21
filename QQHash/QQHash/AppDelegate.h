154e62ed43f10f13fa0794ccb2e1b0bfabc1b3d0
wanghongqing <272338444@qq.com>
2017-12-21 14:14:46 +0800

    初始化代码
    初始化代码

---
+++ QQHash/QQHash/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQHash
+//
+//  Created by Mac on 21/12/2017.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQHash/QQHash/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QQHash
+//
+//  Created by Mac on 21/12/2017.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
